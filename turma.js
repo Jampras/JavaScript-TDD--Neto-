961919d557fd082833aa734549e23ef3fe3f01bf
Isabela de França Lima <isabela.franca.lima@gmail.com>
2026-08-23 13:43:49 -0300

    AdicionarAluno 1 - PASS

--- turma.js
+++ turma.js
@@ -8,35 +8,6 @@ class Turma {
     this.alunos.push(aluno);
     return aluno;
   }
-
-  calcularMediaGeral() {
-    let somaMedias = 0;
-
-    for (const aluno of this.alunos) {
-      somaMedias += aluno.calcularMedia();
-    }
-
-    return somaMedias / this.alunos.length;
-  }
-
-  obterEstatisticas() {
-    let aprovados = 0;
-    let reprovados = 0;
-
-    for (const aluno of this.alunos) {
-      if (aluno.obterSituacao() === "Aprovado") {
-        aprovados++;
-      } else {
-        reprovados++;
-      }
-    }
-
-    return {
-      mediaGeral: this.calcularMediaGeral(),
-      aprovados,
-      reprovados,
-    };
-  }
 }
 
 export default Turma;
